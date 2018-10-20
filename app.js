6f6ba29d07269cc95e2f4cd3766bd99edcb668ad
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-10-20 15:51:07 +0000

    Express-Remember

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-var app;
+const express = require('express')
+const app = express();
+
+
+app.get('/', function(req, res){
+    res.send('Hello World')
+    res.end();
+})
+
+app.listen(3000, function(){
+    console.log('Listening to port 3000, Created by me by the way');
+})
